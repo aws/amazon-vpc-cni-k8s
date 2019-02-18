f4d8a98fbdd8bda5917ab2de43ef97f1b575081c
Andreas Kohn <andreas.kohn@gmail.com>
2019-02-18 12:42:01 +0100

    Fix tiny typo in log message
    Consistently use '<comma><space>' to separate values in log messages, not '<space><comma>'.

--- plugins/routed-eni/cni.go
+++ plugins/routed-eni/cni.go
@@ -161,7 +161,7 @@ func add(args *skel.CmdArgs, cniTypes typeswrapper.CNITYPES, grpcClient grpcwrap
 		return fmt.Errorf("add cmd: failed to assign an IP address to container")
 	}
 
-	log.Infof("Received add network response for pod %s namespace %s container %s: %s, table %d ,external-SNAT: %v, vpcCIDR: %v",
+	log.Infof("Received add network response for pod %s namespace %s container %s: %s, table %d, external-SNAT: %v, vpcCIDR: %v",
 		string(k8sArgs.K8S_POD_NAME), string(k8sArgs.K8S_POD_NAMESPACE), string(k8sArgs.K8S_POD_INFRA_CONTAINER_ID),
 		r.IPv4Addr, r.DeviceNumber, r.UseExternalSNAT, r.VPCcidrs)
 
