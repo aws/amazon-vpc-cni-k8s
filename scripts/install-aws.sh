e1d127c400eb5211ed4bffe06ca4120003b44816
Claes Mogren <mogren@amazon.com>
2019-08-06 14:34:39 -0700

    Copy the binary and config after ipamd is ready
    (cherry picked from commit f9acdeb62644b68cd2c94ef2725ffcbc64fbec76)

--- scripts/install-aws.sh
+++ scripts/install-aws.sh
@@ -1,47 +1,12 @@
 #!/usr/bin/env bash
+echo "====== Installing AWS-CNI ======"
+sed -i s/__VETHPREFIX__/${AWS_VPC_K8S_CNI_VETHPREFIX:-"eni"}/g /app/10-aws.conflist
+cp /app/portmap /host/opt/cni/bin/
+cp /app/aws-cni-support.sh /host/opt/cni/bin/
 
-set -uo pipefail
-
-grpcHealthCheck () {
-  /app/grpc_health_probe -addr 127.0.0.1:50051 > /dev/null 2>&1
-}
-
-waitIPamDServing () {
-   until grpcHealthCheck; do
-    echo "Waiting for ipamd health check";
-    sleep 1;
-  done
-  echo "Ipamd is up and serving"
-}
-
-main () {
-  # Remove old config files that might have been baked into older AMIs
-  if [[ -f /host/etc/cni/net.d/aws.conf ]]; then
+if [[ -f /host/etc/cni/net.d/aws.conf ]]; then
     rm /host/etc/cni/net.d/aws.conf
-  fi
+fi
 
-  # Copy standard files
-  cp /app/aws-cni-support.sh /host/opt/cni/bin/
-  cp /app/portmap /host/opt/cni/bin/
-
-  echo "====== Starting amazon-k8s-agent ======"
-  /app/aws-k8s-agent &
-
-  # Check ipamd health
-  echo "Checking if ipamd is serving"
-  waitIPamDServing
-
-  echo "Copying AWS CNI plugin and config"
-  sed -i s/__VETHPREFIX__/"${AWS_VPC_K8S_CNI_VETHPREFIX:-"eni"}"/g /app/10-aws.conflist
-  cp /app/aws-cni /host/opt/cni/bin/
-  cp /app/10-aws.conflist /host/etc/cni/net.d/
-
-  echo "Node ready, watching ipamd health"
-  # Check that ipamd is healthy, exit if the check fails
-  while grpcHealthCheck; do
-      sleep 10;
-  done
-  echo "Ipamd health check failed, exiting..."
-}
-
-main
+echo "====== Starting amazon-k8s-agent ======"
+/app/aws-k8s-agent
