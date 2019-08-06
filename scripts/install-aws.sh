86e6156cbd99ebd0724f3473c337ca2c663c28cc
Claes Mogren <mogren@amazon.com>
2019-08-06 14:34:39 -0700

    Copy the binary and config after ipamd is ready

--- scripts/install-aws.sh
+++ scripts/install-aws.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
-echo "===== Starting installing AWS-CNI ========="
+echo "====== Installing AWS-CNI ======"
 sed -i s/__VETHPREFIX__/${AWS_VPC_K8S_CNI_VETHPREFIX:-"eni"}/g /app/10-aws.conflist
-cp /app/aws-cni /host/opt/cni/bin/
 cp /app/portmap /host/opt/cni/bin/
 cp /app/aws-cni-support.sh /host/opt/cni/bin/
-cp /app/10-aws.conflist /host/etc/cni/net.d/
 
 if [[ -f /host/etc/cni/net.d/aws.conf ]]; then
     rm /host/etc/cni/net.d/aws.conf
 fi
 
-echo "===== Starting amazon-k8s-agent ==========="
+echo "====== Starting amazon-k8s-agent ======"
 /app/aws-k8s-agent
