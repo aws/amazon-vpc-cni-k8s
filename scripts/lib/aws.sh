d1f44b129555db18d8a7d50fddcb3386148f9cc5
Jay Pipes <jaypipes@gmail.com>
2019-12-19 17:48:50 -0500

    do pre-checks before creating EKS test cluster
    Ensure that there is an ECR repo before we use aws-k8s-tester to actually
    create the test cluster. In addition, trap errors in the main
    run-integration-tests.sh script and deprovision cluster if an error
    occurred after creating the cluster.
    
    Updates the required aws-k8s-tester release to 0.5.2 to handle
    aws/aws-k8s-tester#66
    
    Issue #765
    
    (cherry picked from commit 45a3719a31d51af78cd4f5301c8b1d0a67833990)

--- scripts/lib/aws.sh
+++ scripts/lib/aws.sh
@@ -5,21 +5,24 @@ check_aws_credentials() {
         ( echo "No AWS credentials found. Please run \`aws configure\` to set up the CLI for your credentials." && exit 1)
 }
 
-check_ecr_repo_exists() {
+ensure_ecr_repo() {
     local __registry_account_id="$1"
     local __repo_name="$2"
-    aws ecr describe-repositories --registry-id "$__registry_account_id" --repository-names "$__repo_name" >/dev/null 2>&1 ||
-        ( echo "Could not find ECR repo with name $__repo_name in registry account $__registry_account_id" && exit 1 )
+    if ! `aws ecr describe-repositories --registry-id "$__registry_account_id" --repository-names "$__repo_name" >/dev/null 2>&1`; then
+        echo "creating ECR repo with name $__repo_name in registry account $__registry_account_id"
+        aws ecr create-repository --repository-name "$__repo_name"
+    fi
 }
 
 ensure_aws_k8s_tester() {
-    TESTER_DOWNLOAD_URL=https://github.com/aws/aws-k8s-tester/releases/download/v0.4.3/aws-k8s-tester-v0.4.3-$OS-$ARCH
+    TESTER_RELEASE=${TESTER_RELEASE:-v0.5.2}
+    TESTER_DOWNLOAD_URL=https://github.com/aws/aws-k8s-tester/releases/download/$TESTER_RELEASE/aws-k8s-tester-$TESTER_RELEASE-$OS-$ARCH
 
     # Download aws-k8s-tester if not yet
     if [[ ! -e $TESTER_PATH ]]; then
-      mkdir -p $TESTER_DIR
-      echo "Downloading aws-k8s-tester from $TESTER_DOWNLOAD_URL to $TESTER_PATH"
-      curl -s -L -X GET $TESTER_DOWNLOAD_URL -o $TESTER_PATH
-      chmod +x $TESTER_PATH
+        mkdir -p $TESTER_DIR
+        echo "Downloading aws-k8s-tester from $TESTER_DOWNLOAD_URL to $TESTER_PATH"
+        curl -s -L -X GET $TESTER_DOWNLOAD_URL -o $TESTER_PATH
+        chmod +x $TESTER_PATH
     fi
 }
