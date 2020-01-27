e9cd0d11fe68843b4c28424199408a96e2e689db
Claes Mogren <mogren@amazon.com>
2020-01-27 15:18:38 -0800

    Add integration_test job to CircleCI config

--- scripts/lib/cluster.sh
+++ scripts/lib/cluster.sh
@@ -1,15 +1,19 @@
 #!/usr/bin/env bash
 
 function down-test-cluster() {
+    if [[ -n "${CIRCLE_JOB:-}" ]]; then
+        $TESTER_PATH eks delete cluster --path $CLUSTER_CONFIG || (echo "failed!" && exit 1)
+    else
     echo -n "Deleting cluster $CLUSTER_NAME (this may take ~10 mins) ... "
-    $TESTER_PATH eks delete cluster --path $CLUSTER_CONFIG >>$CLUSTER_MANAGE_LOG_PATH 2>&1 ||
-        ( echo "failed. Check $CLUSTER_MANAGE_LOG_PATH." && exit 1 )
+    $TESTER_PATH eks delete cluster --path $CLUSTER_CONFIG >> $CLUSTER_MANAGE_LOG_PATH 2>&1 ||
+        (echo "failed. Check $CLUSTER_MANAGE_LOG_PATH." && exit 1)
     echo "ok."
+    fi
 }
 
 function up-test-cluster() {
+    echo -n "Configuring cluster $CLUSTER_NAME"
     ssh-keygen -q -P cni-test -f $SSH_KEY_PATH
-
     AWS_K8S_TESTER_EKS_CLUSTER_NAME=$CLUSTER_NAME \
         AWS_K8S_TESTER_EKS_KUBECONFIG_PATH=$KUBECONFIG_PATH \
         AWS_K8S_TESTER_EKS_KUBERNETES_VERSION=${K8S_VERSION%.*} \
@@ -28,9 +32,14 @@ function up-test-cluster() {
         AWS_K8S_TESTER_EKS_ADD_ON_NLB_HELLO_WORLD_ENABLE=true \
         AWS_K8S_TESTER_EKS_ADD_ON_ALB_2048_ENABLE=true \
         AWS_K8S_TESTER_EKS_KUBECTL_PATH=$KUBECTL_PATH \
-    $TESTER_PATH eks create config --path $CLUSTER_CONFIG 1>&2
-    echo -n "Creating cluster $CLUSTER_NAME (this may take ~20 mins. details: tail -f $CLUSTER_MANAGE_LOG_PATH)... "
-    $TESTER_PATH eks create cluster --path $CLUSTER_CONFIG 2>>$CLUSTER_MANAGE_LOG_PATH 1>&2 ||
-        ( echo "failed. Check $CLUSTER_MANAGE_LOG_PATH." && exit 1 )
-    echo "ok."
+        $TESTER_PATH eks create config --path $CLUSTER_CONFIG 1>&2
+
+    if [[ -n "${CIRCLE_JOB:-}" ]]; then
+        $TESTER_PATH eks create cluster --path $CLUSTER_CONFIG || (echo "failed!" && exit 1)
+    else
+        echo -n "Creating cluster $CLUSTER_NAME (this may take ~20 mins. details: tail -f $CLUSTER_MANAGE_LOG_PATH)... "
+        $TESTER_PATH eks create cluster --path $CLUSTER_CONFIG 2>> $CLUSTER_MANAGE_LOG_PATH 1>&2 ||
+            (echo "failed. Check $CLUSTER_MANAGE_LOG_PATH." && exit 1)
+        echo "ok."
+    fi
 }
