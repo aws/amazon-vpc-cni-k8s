581de34cb08202a39c48ebfba15f4b580535fb1e
Claes Mogren <mogren@amazon.com>
2020-02-22 01:37:54 -0800

    Fix file copyright header to comply with directives

--- pkg/awsutils/gen_vpc_ip_limits.go
+++ pkg/awsutils/gen_vpc_ip_limits.go
@@ -1,4 +1,4 @@
-// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License"). You may
 // not use this file except in compliance with the License. A copy of the
@@ -140,7 +140,7 @@ func newENILimit(instanceType string, eniLimit int64, ipLimit int64) ENILimit {
 	}
 }
 
-var limitsTemplate = template.Must(template.New("").Parse(`// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+var limitsTemplate = template.Must(template.New("").Parse(`// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License"). You may
 // not use this file except in compliance with the License. A copy of the
