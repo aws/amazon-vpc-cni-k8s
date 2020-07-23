9b4771da630a9ccf2cff2138b00212cb9cd368c0
Ben Napolitan <bnapolitan@outlook.com>
2020-07-23 03:18:35 -0400

    Don't run performance tests.

--- scripts/run-performance-and-release-tests.sh
+++ scripts/run-performance-and-release-tests.sh
@@ -13,9 +13,6 @@ export RUN_WARM_ENI_TEST=true
 unset 
 
 export RUN_CONFORMANCE=false
-export RUN_PERFORMANCE_TESTS=true
-./scripts/run-integration-tests.sh
-unset RUN_PERFORMANCE_TESTS
 
 export RUN_KOPS_TEST=true
 ./scripts/run-integration-tests.sh
