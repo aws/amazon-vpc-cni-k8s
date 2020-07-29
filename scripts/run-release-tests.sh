b1ff7ff818949f06e7902aeb72ad8bb1c456c9b4
Ben Napolitan <bnapolitan@outlook.com>
2020-07-29 11:31:20 -0400

    Add echo to show which test is running.

--- scripts/run-release-tests.sh
+++ scripts/run-release-tests.sh
@@ -1,19 +1,22 @@
 export RUN_BOTTLEROCKET_TEST=true
 chmod +x ./scripts/run-integration-tests.sh
-
+echo "Running bottlerocket test"
 ./scripts/run-integration-tests.sh
 unset RUN_BOTTLEROCKET_TEST
 
 export RUN_WARM_IP_TEST=true
+echo "Running warm ip test"
 ./scripts/run-integration-tests.sh
 unset RUN_WARM_IP_TEST
 
 export RUN_WARM_ENI_TEST=true
+echo "Running warm eni test"
 ./scripts/run-integration-tests.sh
 unset RUN_WARM_ENI_TEST
 
 export RUN_CONFORMANCE=false
 
 export RUN_KOPS_TEST=true
+echo "Running KOPS test"
 ./scripts/run-integration-tests.sh
 unset RUN_KOPS_TEST
