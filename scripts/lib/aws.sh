dfaf7bd8d70008fe52f447f55b348c5d166fb47f
Jayanth Varavani <1111446+jayanthvn@users.noreply.github.com>
2020-09-21 19:24:05 +0000

    update k8s tester

--- scripts/lib/aws.sh
+++ scripts/lib/aws.sh
@@ -16,7 +16,7 @@ ensure_ecr_repo() {
 }
 
 ensure_aws_k8s_tester() {
-    TESTER_RELEASE=${TESTER_RELEASE:-v1.4.8}
+    TESTER_RELEASE=${TESTER_RELEASE:-v1.5.2}
     TESTER_DOWNLOAD_URL=https://github.com/aws/aws-k8s-tester/releases/download/$TESTER_RELEASE/aws-k8s-tester-$TESTER_RELEASE-$OS-$ARCH
 
     # Download aws-k8s-tester if not yet
