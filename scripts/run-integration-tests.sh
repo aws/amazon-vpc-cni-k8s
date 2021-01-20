b72343fae73fcdc3182759174e1d392e282d00a5
Jayanth Varavani <1111446+jayanthvn@users.noreply.github.com>
2021-01-20 22:00:55 +0000

    revert k8s version since it broke circle ci

--- scripts/run-integration-tests.sh
+++ scripts/run-integration-tests.sh
@@ -16,7 +16,7 @@ OS=$(go env GOOS)
 ARCH=$(go env GOARCH)
 
 : "${AWS_DEFAULT_REGION:=us-west-2}"
-: "${K8S_VERSION:=1.18.14}"
+: "${K8S_VERSION:=1.16.10}"
 : "${PROVISION:=true}"
 : "${DEPROVISION:=true}"
 : "${BUILD:=true}"
