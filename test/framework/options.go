895bd9da50e0699d229b78e50a7e588c93d5b9a1
Abhinav Pathak <31713659+abhipth@users.noreply.github.com>
2021-04-08 14:39:08 -0700

    add ginkgo automation framework (#1416)

---
+++ test/framework/options.go
@@ -0,0 +1,68 @@
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"). You may
+// not use this file except in compliance with the License. A copy of the
+// License is located at
+//
+//     http://aws.amazon.com/apache2.0/
+//
+// or in the "license" file accompanying this file. This file is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+package framework
+
+import (
+	"flag"
+
+	"github.com/pkg/errors"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+var GlobalOptions Options
+
+func init() {
+	GlobalOptions.BindFlags()
+}
+
+type Options struct {
+	KubeConfig     string
+	ClusterName    string
+	AWSRegion      string
+	AWSVPCID       string
+	NgNameLabelKey string
+	NgNameLabelVal string
+}
+
+func (options *Options) BindFlags() {
+	flag.StringVar(&options.KubeConfig, "cluster-kubeconfig", "", "Path to kubeconfig containing embedded authinfo (required)")
+	flag.StringVar(&options.ClusterName, "cluster-name", "", `Kubernetes cluster name (required)`)
+	flag.StringVar(&options.AWSRegion, "aws-region", "", `AWS Region for the kubernetes cluster`)
+	flag.StringVar(&options.AWSVPCID, "aws-vpc-id", "", `AWS VPC ID for the kubernetes cluster`)
+	flag.StringVar(&options.NgNameLabelKey, "ng-name-label-key", "eks.amazonaws.com/nodegroup", "label key used to identify nodegroup name")
+	flag.StringVar(&options.NgNameLabelVal, "ng-name-label-val", "", "label value with the nodegroup name")
+}
+
+func (options *Options) Validate() error {
+	if len(options.KubeConfig) == 0 {
+		return errors.Errorf("%s must be set!", clientcmd.RecommendedConfigPathFlag)
+	}
+	if len(options.ClusterName) == 0 {
+		return errors.Errorf("%s must be set!", "cluster-name")
+	}
+	if len(options.AWSRegion) == 0 {
+		return errors.Errorf("%s must be set!", "aws-region")
+	}
+	if len(options.AWSVPCID) == 0 {
+		return errors.Errorf("%s must be set!", "aws-vpc-id")
+	}
+	if len(options.NgNameLabelKey) == 0 {
+		return errors.Errorf("%s must be set!", "ng-name-label-key")
+	}
+	if len(options.NgNameLabelVal) == 0 {
+		return errors.Errorf("%s must be set!", "ng-name-label-val")
+	}
+
+	return nil
+}
---
+++ test/framework/resources/k8s/resources/job.go
@@ -0,0 +1,61 @@
+// Copyright Amazon.com Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"). You may
+// not use this file except in compliance with the License. A copy of the
+// License is located at
+//
+//     http://aws.amazon.com/apache2.0/
+//
+// or in the "license" file accompanying this file. This file is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+package resources
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/aws/amazon-vpc-cni-k8s/test/framework/utils"
+
+	v1 "k8s.io/api/batch/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+type JobManager interface {
+	CreateAndWaitTillJobCompleted(job *v1.Job) (*v1.Job, error)
+}
+
+type defaultJobManager struct {
+	k8sClient client.DelegatingClient
+}
+
+func NewDefaultJobManager(k8sClient client.DelegatingClient) JobManager {
+	return &defaultJobManager{k8sClient: k8sClient}
+}
+
+func (d *defaultJobManager) CreateAndWaitTillJobCompleted(job *v1.Job) (*v1.Job, error) {
+	ctx := context.Background()
+	err := d.k8sClient.Create(ctx, job)
+	if err != nil {
+		return nil, err
+	}
+
+	time.Sleep(utils.PollIntervalShort)
+
+	observedJob := &v1.Job{}
+	return observedJob, wait.PollImmediateUntil(utils.PollIntervalShort, func() (bool, error) {
+		if err := d.k8sClient.Get(ctx, utils.NamespacedName(job), observedJob); err != nil {
+			return false, err
+		}
+		if observedJob.Status.Failed > 0 {
+			return false, fmt.Errorf("failed to execute job :%v", observedJob.Status)
+		} else if observedJob.Status.Succeeded == (*job.Spec.Parallelism) {
+			return true, nil
+		}
+		return false, nil
+	}, ctx.Done())
+}
