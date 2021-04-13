35b78292f3dc5560f00d3ab37083439b928adcbd
Diogo Nicoleti <diogo.nicoleti@nubank.com.br>
2021-04-13 12:43:21 -0300

    Add version package and build_info metric

--- cmd/aws-k8s-agent/main.go
+++ cmd/aws-k8s-agent/main.go
@@ -21,9 +21,8 @@ import (
 	"github.com/aws/amazon-vpc-cni-k8s/pkg/ipamd"
 	"github.com/aws/amazon-vpc-cni-k8s/pkg/k8sapi"
 	"github.com/aws/amazon-vpc-cni-k8s/pkg/utils/logger"
+	"github.com/aws/amazon-vpc-cni-k8s/pkg/version"
 )
-
-var version string
 
 func main() {
 	os.Exit(_main())
@@ -37,7 +36,8 @@ func _main() int {
 	}
 	log := logger.New(&logConfig)
 
-	log.Infof("Starting L-IPAMD %s  ...", version)
+	log.Infof("Starting L-IPAMD %s  ...", version.Version)
+	version.RegisterMetric()
 
 	kubeClient, err := k8sapi.CreateKubeClient()
 	if err != nil {
@@ -67,7 +67,7 @@ func _main() int {
 	go ipamContext.ServeIntrospection()
 
 	// Start the RPC listener
-	err = ipamContext.RunRPCHandler(version)
+	err = ipamContext.RunRPCHandler(version.Version)
 	if err != nil {
 		log.Errorf("Failed to set up gRPC handler: %v", err)
 		return 1
