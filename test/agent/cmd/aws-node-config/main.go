3b4c92f2e11f197bfaaf9b80dd15ce242b12dccd
Chinmay Gadgil <cgadgil@amazon.com>
2021-05-12 11:26:27 -0700

    Added base64 deserialization logic Improved logic to test veth pairs

--- test/agent/cmd/aws-node-config/main.go
+++ test/agent/cmd/aws-node-config/main.go
@@ -13,11 +13,17 @@
 package main
 
 import (
+	"crypto/sha1"
+	"encoding/base64"
+	"encoding/hex"
+	"encoding/json"
 	"flag"
 	"fmt"
 	"log"
 	"net"
 	"strings"
+
+	"github.com/aws/amazon-vpc-cni-k8s/test/agent/pkg/input"
 )
 
 const (
@@ -25,68 +31,126 @@ const (
 	VETH_PREFIX_CHECK = "veth"
 )
 
+type MTU_CONFIG struct {
+	ExpectedVal int
+}
+
 func main() {
 	validate := ""
-	prefix := "eni"
-	vethCount := 1
-	mtuVal := 9001
+	config := ""
 
 	flag.StringVar(&validate, "validate", "", "name of the test, valid values: mtu, veth")
-	flag.StringVar(&prefix, "prefix", "eni", "veth prefix to be used, specify when validating veth")
-	flag.IntVar(&vethCount, "veth-count", 1, "Number of expected veth links to have the new prefix. specify when validating veth")
-	flag.IntVar(&mtuVal, "mtu-val", 9001, "Expected MTU values, specify when validating mtu")
+	flag.StringVar(&config, "config", "", "Base64 encoded json object for the given test")
 	flag.Parse()
+
+	decoded, err := base64.StdEncoding.DecodeString(config)
+	if err != nil {
+		log.Fatalf("failed to descode base64 string %s: %v", config, err)
+	}
 
 	switch validate {
 	case MTU_CHECK:
-		validateMTUValue(mtuVal)
+		validateMTUValue(decoded)
 	case VETH_PREFIX_CHECK:
-		validateVethPairCount(prefix, vethCount)
+		validateVethPairCount(decoded)
 	default:
 		fmt.Println("Invalid test-name choice")
 	}
 }
 
-func validateVethPairCount(prefix string, expected int) {
+func validateVethPairCount(config []byte) {
+	var podNetworkingValidationInput input.PodNetworkingValidationInput
+	err := json.Unmarshal(config, &podNetworkingValidationInput)
+	if err != nil {
+		log.Fatalf("failed to unmarshall json string %s: %v", config, err)
+	}
+
+	log.Printf("podNetworkingValidationInput: %+v\n", podNetworkingValidationInput)
+
+	if podNetworkingValidationInput.VethPrefix == "" {
+		podNetworkingValidationInput.VethPrefix = "eni"
+	}
+
+	actual := 0
+	vethPairs := make(map[string]bool, len(podNetworkingValidationInput.PodList))
+
+	for _, pod := range podNetworkingValidationInput.PodList {
+		// Get the veth pair for pod in host network namespace
+		hostVethName := getHostVethPairName(pod, podNetworkingValidationInput.VethPrefix)
+		log.Printf("hostVethName: %v\n", hostVethName)
+		vethPairs[hostVethName] = false
+	}
+
 	interfaces, err := net.Interfaces()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	actual := 0
 	for _, ni := range interfaces {
-		if strings.HasPrefix(ni.Name, prefix) {
-			actual++
+		if checked, ok := vethPairs[ni.Name]; ok {
+			if !checked {
+				actual++
+			}
 		}
 	}
 
-	if actual >= expected {
+	if actual == len(podNetworkingValidationInput.PodList) {
 		log.Println("Veth Check: Passed")
 	} else {
 		log.Println("Veth Check: Failed")
+		log.Println("Missing Veth pairs")
+		for name, val := range vethPairs {
+			if !val {
+				log.Println(name)
+			}
+		}
 	}
 }
 
-func validateMTUValue(expected int) {
+func validateMTUValue(config []byte) {
+	mtuConfig := MTU_CONFIG{}
+	err := json.Unmarshal(config, &mtuConfig)
+
+	if err != nil {
+		log.Fatalf("failed to unmarshall json string %s: %v", config, err)
+	}
+
+	log.Printf("MTU_CONFIG: %+v\n", mtuConfig)
+
+	if mtuConfig.ExpectedVal == 0 {
+		mtuConfig.ExpectedVal = 9001
+	}
+
 	interfaces, err := net.Interfaces()
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	ok := false
+	failedInterfaces := make([]string, 0)
 	for _, ni := range interfaces {
 		if strings.HasPrefix(ni.Name, "eth") {
-			if ni.MTU != expected {
-				log.Fatal("MTU Check: Failed")
-			} else {
-				ok = true
+			if ni.MTU != mtuConfig.ExpectedVal {
+				failedInterfaces = append(failedInterfaces, ni.Name)
 			}
+			ok = true
 		}
 	}
 	if !ok {
 		log.Println("MTU Check: Failed")
 		log.Println("Could not find eth interface, check ifconfig on the node")
 	} else {
-		log.Println("MTU Check: Passed")
+		if len(failedInterfaces) != 0 {
+			log.Println("MTU Check: Failed")
+			log.Printf("Failed Interfaces: %+v\n", failedInterfaces)
+		} else {
+			log.Println("MTU Check: Passed")
+		}
 	}
+}
+
+func getHostVethPairName(input input.Pod, vethPrefix string) string {
+	h := sha1.New()
+	h.Write([]byte(fmt.Sprintf("%s.%s", input.PodNamespace, input.PodName)))
+	return fmt.Sprintf("%s%s", vethPrefix, hex.EncodeToString(h.Sum(nil))[:11])
 }
