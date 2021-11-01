2e2522f42405f2e3f3115d179f466c3b98f1e2ee
Abhinav Pathak <abhipth@amazon.com>
2021-11-01 14:35:47 -0700

    run tests only on the latest addon version

--- scripts/run-canary-test.sh
+++ scripts/run-canary-test.sh
@@ -96,17 +96,27 @@ function run_ginkgo_test() {
 load_cluster_details
 load_addon_details
 
+# TODO: v1.7.5 restarts continiously if IMDS goes out of sync, the issue is mitigated
+# from v.1.8.0 onwards, once the default addon is updated to v1.8.0+ we should uncomment
+# the following code. See: https://github.com/aws/amazon-vpc-cni-k8s/issues/1340
+
 # Run more comprehensive test on the default addon version. CANARY focused tests
 # tests basic functionlity plus test that could detect issues with dependencies
 # early on.
-echo "Running Canary tests on the default addon version"
-install_add_on "$DEFAULT_ADDON_VERSION"
-run_ginkgo_test "CANARY"
+#echo "Running Canary tests on the default addon version"
+#install_add_on "$DEFAULT_ADDON_VERSION"
+#run_ginkgo_test "CANARY"
 
 # Run smoke test on the latest addon version. Smoke tests contains a subset of test
 # used in Canary tests.
-echo "Running Smoke tests on the latest addon version"
+#echo "Running Smoke tests on the latest addon version"
+#install_add_on "$LATEST_ADDON_VERSION"
+#run_ginkgo_test "SMOKE"
+
+# TODO: Remove the following code once the v1.8.0+ is made the default addon version
+echo "Running Canary tests on the latest addon version"
 install_add_on "$LATEST_ADDON_VERSION"
-run_ginkgo_test "SMOKE"
+run_ginkgo_test "CANARY"
+
 
 echo "all tests ran successfully in $(($SECONDS / 60)) minutes and $(($SECONDS % 60)) seconds"
