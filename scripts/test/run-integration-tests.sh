f87037a0fc9fb0f8a74746fcf53322a300a0da44
Shreya Anil Naik <shrenaik@amazon.com>
2021-12-01 00:52:27 +0530

    Add ENDPOINT env var, TEST_FAILED status and other minor refactoring

--- scripts/test/run-integration-tests.sh
+++ scripts/test/run-integration-tests.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-set -e
-OS_OVERRIDE=$(go env GOOS)
+set -eo pipefail
+
 pushd ./test/integration-new
 
 echo "Running integration test with the following configuration:
-KUBE CONFIG: $KUBECONFIG
+KUBECONFIG: $KUBECONFIG
 CLUSTER_NAME: $CLUSTER_NAME
-AWS_REGION: $AWS_REGION
-OS_OVERRIDE: $OS_OVERRIDE"
+AWS_REGION: $AWS_REGION"
+
+
+if [[ -n "${ENDPOINT}" ]]; then
+  ENDPOINT_FLAG="--endpoint $ENDPOINT"
+fi
 
 while getopts "f:" arg; do
   case $arg in
@@ -16,20 +20,14 @@ while getopts "f:" arg; do
     echo "FOCUS: $FOCUS";
   esac
 done
-    
 
-if [[ -z "${OS_OVERRIDE}" ]]; then
-    OS_OVERRIDE=linux
-fi
-
-CLUSTER_INFO=$(aws eks describe-cluster --name $CLUSTER_NAME --region $AWS_REGION)
+CLUSTER_INFO=$(aws eks describe-cluster --name $CLUSTER_NAME --region $AWS_REGION $ENDPOINT_FLAG)
 
 VPC_ID=$(echo $CLUSTER_INFO | jq -r '.cluster.resourcesVpcConfig.vpcId')
 SERVICE_ROLE_ARN=$(echo $CLUSTER_INFO | jq -r '.cluster.roleArn')
 ROLE_NAME=${SERVICE_ROLE_ARN##*/}
+TEST_FAILED=false
  
-
-
 START=$SECONDS
 
 for dir in */  
@@ -37,16 +35,20 @@ do
 
     cd "${dir%*/}"  
     if [ -n "$FOCUS" ]; then
-        ginkgo --focus="$FOCUS" -v -r -- --cluster-kubeconfig=$KUBECONFIG --cluster-name=$CLUSTER_NAME --aws-region=$AWS_REGION --aws-vpc-id=$VPC_ID
+        ( ginkgo --focus="$FOCUS" -v -r -- --cluster-kubeconfig=$KUBECONFIG --cluster-name=$CLUSTER_NAME --aws-region=$AWS_REGION --aws-vpc-id=$VPC_ID ) || TEST_FAILED=true
     else
-        ginkgo -v -r -- --cluster-kubeconfig=$KUBECONFIG --cluster-name=$CLUSTER_NAME --aws-region=$AWS_REGION --aws-vpc-id=$VPC_ID
+        ( ginkgo -v -r -- --cluster-kubeconfig=$KUBECONFIG --cluster-name=$CLUSTER_NAME --aws-region=$AWS_REGION --aws-vpc-id=$VPC_ID ) || TEST_FAILED=true
     fi
     cd ..    
      
 done
 
-
 DEFAULT_INTEGRATION_DURATION=$((SECONDS - START))
 echo "TIMELINE: Integration tests took $DEFAULT_INTEGRATION_DURATION seconds."
 
 popd
+
+# If any of the test failed, exit with non zero exit code
+if [ $TEST_FAILED = true ]; then
+  exit 1
+fi
