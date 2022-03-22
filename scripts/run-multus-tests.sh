4d8dc38f68ffe1fb899a39456bb96b19366d8fc7
Chinmay Gadgil <cgadgil@amazon.com>
2022-03-22 10:14:06 -0700

    Cleaned up run-multus-tests helper script

--- scripts/run-multus-tests.sh
+++ scripts/run-multus-tests.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-# The script runs amazon-vpc-cni Canary tests on the default
-# addon version and then runs smoke test on the latest addon version.
+# The script runs multus tests on the lastest version with latest AWS-VPC-CNI addon version
 
 set -e
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 INTEGRATION_TEST_DIR="$SCRIPT_DIR/../test/integration-new"
 
-source "$SCRIPT_DIR"/lib/add-on.sh
 source "$SCRIPT_DIR"/lib/cluster.sh
 source "$SCRIPT_DIR"/lib/canary.sh
 
 function run_ginkgo_test() {
-  echo "Running multus ginkgo tests"
   (cd "$INTEGRATION_TEST_DIR/multus" && CGO_ENABLED=0 ginkgo -v --timeout 20m --failOnPending -- --cluster-kubeconfig="$KUBE_CONFIG_PATH" --cluster-name="$CLUSTER_NAME" --aws-region="$REGION" --aws-vpc-id="$VPC_ID" --ng-name-label-key="kubernetes.io/os" --ng-name-label-val="linux")
 }
 
 load_cluster_details
-load_addon_details
 
-echo "Running Multus tests on the latest vpc-cni addon version"
-install_add_on "$LATEST_ADDON_VERSION"
+LATEST_TAG="v3.7.2-eksbuild.2"
+echo "Installing latest multus manifest with tag: ${LATEST_TAG}"
+
+kubectl apply -f https://raw.githubusercontent.com/aws/amazon-vpc-cni-k8s/master/config/multus/${LATEST_TAG}/aws-k8s-multus.yaml
+
+echo "Running multus ginkgo tests"
 run_ginkgo_test
 
 echo "all tests ran successfully in $(($SECONDS / 60)) minutes and $(($SECONDS % 60)) seconds"
