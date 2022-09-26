ab8d23855bdf7c3afc15add7cde1edabab927b29
Jeff Nelson <jdnelson@amazon.com>
2022-09-26 16:38:15 +0000

    make format changes to pass make check-format

--- pkg/apis/crd/v1alpha1/groupversion_info.go
+++ pkg/apis/crd/v1alpha1/groupversion_info.go
@@ -15,8 +15,8 @@ limitations under the License.
 */
 
 // Package v1alpha1 contains API Schema definitions for the crd v1alpha1 API group
-//+kubebuilder:object:generate=true
-//+groupName=crd.example.com
+// +kubebuilder:object:generate=true
+// +groupName=crd.example.com
 package v1alpha1
 
 import (
