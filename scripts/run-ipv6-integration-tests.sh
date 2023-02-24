29d0394bbe90b20fb5c060386a11f04c5a07660a
Jeffrey Nelson <jdnelson@amazon.com>
2023-02-24 11:42:30 -0600

    add script to run full IPv6 integration test suite (#2277)

---
+++ scripts/run-ipv6-integration-tests.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# The script runs the full IPv6 integration test suite
+
+set -e
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+GINKGO_TEST_BUILD="$SCRIPT_DIR/../test/build"
+
+source "$SCRIPT_DIR"/lib/add-on.sh
+source "$SCRIPT_DIR"/lib/cluster.sh
+source "$SCRIPT_DIR"/lib/canary.sh
+
+function run_ginkgo_test() {
+  (CGO_ENABLED=0 ginkgo $EXTRA_GINKGO_FLAGS -v --timeout 30m --no-color --fail-on-pending $GINKGO_TEST_BUILD/ipv6.test -- --cluster-kubeconfig="$KUBE_CONFIG_PATH" --cluster-name="$CLUSTER_NAME" --aws-region="$REGION" --aws-vpc-id="$VPC_ID" --ng-name-label-key="kubernetes.io/os" --ng-name-label-val="linux")
+}
+
+load_cluster_details
+load_addon_details
+
+echo "Running IPv6 integration tests against the latest addon version"
+
+install_add_on "$LATEST_ADDON_VERSION"
+run_ginkgo_test
+
+echo "all tests ran successfully in $(($SECONDS / 60)) minutes and $(($SECONDS % 60)) seconds"
