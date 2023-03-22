829147fda063e6d7d2514cfc82d24cb526c87564
Jihun Seol <jihun.seol@samsung.com>
2023-03-22 10:39:07 +0900

    add unit test code for custom endpoint

--- pkg/awsutils/awssession/session_test.go
+++ pkg/awsutils/awssession/session_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/aws/aws-sdk-go/service/ec2"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -20,4 +21,19 @@ func TestHttpTimeoutWithValueAbove10(t *testing.T) {
 	defer os.Unsetenv(httpTimeoutEnv)
 	expectedHTTPTimeOut := time.Duration(12) * time.Second
 	assert.Equal(t, expectedHTTPTimeOut, getHTTPTimeout())
+}
+
+func TestAwsEc2EndpointResolver(t *testing.T) {
+	customEndpoint := "https://ec2.us-west-2.customaws.com"
+	region := "us-west-2"
+
+	os.Setenv("AWS_EC2_ENDPOINT", customEndpoint)
+	defer os.Unsetenv("AWS_EC2_ENDPOINT")
+
+	sess := New()
+
+	resolvedEndpoint, err := sess.Config.EndpointResolver.EndpointFor(ec2.EndpointsID, region)
+	assert.NoError(t, err)
+	assert.Equal(t, customEndpoint, resolvedEndpoint.URL)
+	assert.Equal(t, region, resolvedEndpoint.SigningRegion)
 }
