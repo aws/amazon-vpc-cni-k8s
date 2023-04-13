32feae6a198e2a96ea916c8da8895b4c82637b3d
Yufei Wang <wanyufe@amazon.com>
2023-04-13 17:55:20 +0000

    fix typo Engress to Egress

--- cmd/aws-vpc-cni/main.go
+++ cmd/aws-vpc-cni/main.go
@@ -48,9 +48,10 @@ import (
 	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 
+	"github.com/containernetworking/cni/pkg/types"
+
 	"github.com/aws/amazon-vpc-cni-k8s/pkg/utils/cniutils"
 	"github.com/aws/amazon-vpc-cni-k8s/utils/cp"
-	"github.com/containernetworking/cni/pkg/types"
 )
 
 const (
@@ -67,7 +68,7 @@ const (
 	defaultEgressPluginLogFile   = "/var/log/aws-routed-eni/egress-plugin.log"
 	defaultPluginLogLevel        = "Debug"
 	defaultEnableIPv6            = "false"
-	defaultEnableEngress         = "true"
+	defaultEnableEgress          = "true"
 	egressIPv4Subnet             = "169.254.172.0/22"
 	egressIPv6Subnet             = "fd00::/8"
 	egressIPv4Dst                = "0.0.0.0/0"
@@ -93,7 +94,7 @@ const (
 	envWarmPrefixTarget      = "WARM_PREFIX_TARGET"
 	envEnBandwidthPlugin     = "ENABLE_BANDWIDTH_PLUGIN"
 	envEnIPv6                = "ENABLE_IPv6"
-	envEnEgress              = "ENABLE_ENGRESS"
+	envEnEgress              = "ENABLE_EGRESS"
 	envRandomizeSNAT         = "AWS_VPC_K8S_CNI_RANDOMIZESNAT"
 	envEnableNftables        = "ENABLE_NFTABLES"
 )
@@ -262,7 +263,7 @@ func generateJSON(jsonFile string, outFile string) error {
 		// EKS IPv4 cluster
 		egressIPSubnet = egressIPv6Subnet
 		egressIPDst = egressIPv6Dst
-		egressEnabled = getEnv(envEnEgress, defaultEnableEngress)
+		egressEnabled = getEnv(envEnEgress, defaultEnableEgress)
 		egressPluginLogFile = getEnv(envEgressPluginLogFile, defaultEgressPluginLogFile)
 		if egressEnabled == "true" {
 			nodeIP, err = getNodePrimaryV6Address()
