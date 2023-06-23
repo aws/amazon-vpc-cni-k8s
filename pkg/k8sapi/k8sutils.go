e5905f9383ae1f410254aff1510bce89b2c7d78c
Sam Lockart <sam.lockart@zendesk.com>
2023-06-23 14:40:39 +1000

    Scope the cached k8s client to the node if possible

--- pkg/k8sapi/k8sutils.go
+++ pkg/k8sapi/k8sutils.go
@@ -8,19 +8,21 @@ import (
 
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/meta"
+	"k8s.io/apimachinery/pkg/fields"
 	"k8s.io/apimachinery/pkg/types"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	"sigs.k8s.io/controller-runtime/pkg/client/apiutil"
 
-	eniconfigscheme "github.com/aws/amazon-vpc-cni-k8s/pkg/apis/crd/v1alpha1"
-	"github.com/aws/amazon-vpc-cni-k8s/pkg/utils/logger"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/cache"
+	crcache "sigs.k8s.io/controller-runtime/pkg/cache"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	eniconfigscheme "github.com/aws/amazon-vpc-cni-k8s/pkg/apis/crd/v1alpha1"
+	"github.com/aws/amazon-vpc-cni-k8s/pkg/utils/logger"
 )
 
 var log = logger.Get()
@@ -45,11 +47,16 @@ func CreateKubeClient(mapper meta.RESTMapper) (client.Client, error) {
 		return nil, err
 	}
 	vpcCniScheme := runtime.NewScheme()
-	clientgoscheme.AddToScheme(vpcCniScheme)
-	eniconfigscheme.AddToScheme(vpcCniScheme)
+	err = clientgoscheme.AddToScheme(vpcCniScheme)
+	if err != nil {
+		return nil, err
+	}
+	err = eniconfigscheme.AddToScheme(vpcCniScheme)
+	if err != nil {
+		return nil, err
+	}
 
 	rawK8SClient, err := client.New(restCfg, client.Options{Scheme: vpcCniScheme, Mapper: mapper})
-
 	if err != nil {
 		return nil, err
 	}
@@ -57,7 +64,8 @@ func CreateKubeClient(mapper meta.RESTMapper) (client.Client, error) {
 	return rawK8SClient, nil
 }
 
-// CreateKubeClient creates a k8s client
+// CreateCachedKubeClient creates a k8s client which caches
+// pods.
 func CreateCachedKubeClient(rawK8SClient client.Client, mapper meta.RESTMapper) (client.Client, error) {
 	restCfg, err := getRestConfig()
 	if err != nil {
@@ -66,16 +74,32 @@ func CreateCachedKubeClient(rawK8SClient client.Client, mapper meta.RESTMapper) 
 	restCfg.Burst = 100
 
 	vpcCniScheme := runtime.NewScheme()
-	clientgoscheme.AddToScheme(vpcCniScheme)
-	eniconfigscheme.AddToScheme(vpcCniScheme)
+	err = clientgoscheme.AddToScheme(vpcCniScheme)
+	if err != nil {
+		return nil, err
+	}
+	err = eniconfigscheme.AddToScheme(vpcCniScheme)
+	if err != nil {
+		return nil, err
+	}
+
+	// we only care about pods on the current node.
+	// selecting every pod leads to high memory usage
+	// on larger clusters.
+	cacheOptions := crcache.Options{Scheme: vpcCniScheme, Mapper: mapper}
+	if nodeName := os.Getenv("MY_NODE_NAME"); nodeName != "" {
+		cacheOptions.SelectorsByObject = map[client.Object]crcache.ObjectSelector{&corev1.Pod{}: {
+			Field: fields.Set{"spec.nodeName": nodeName}.AsSelector(),
+		}}
+	}
 
 	stopChan := ctrl.SetupSignalHandler()
-	cache, err := cache.New(restCfg, cache.Options{Scheme: vpcCniScheme, Mapper: mapper})
+	cache, err := crcache.New(restCfg, cacheOptions)
 	if err != nil {
 		return nil, err
 	}
 	go func() {
-		cache.Start(stopChan)
+		_ = cache.Start(stopChan)
 	}()
 	cache.WaitForCacheSync(stopChan)
 
--- pkg/k8sapi/k8sutils_test.go
+++ pkg/k8sapi/k8sutils_test.go
@@ -5,33 +5,42 @@ import (
 	"os"
 	"testing"
 
-	eniconfigscheme "github.com/aws/amazon-vpc-cni-k8s/pkg/apis/crd/v1alpha1"
 	"github.com/stretchr/testify/assert"
 	corev1 "k8s.io/api/core/v1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+
+	eniconfigscheme "github.com/aws/amazon-vpc-cni-k8s/pkg/apis/crd/v1alpha1"
 )
 
 func TestGetNode(t *testing.T) {
+	var err error
 	ctx := context.Background()
 	k8sSchema := runtime.NewScheme()
-	clientgoscheme.AddToScheme(k8sSchema)
-	eniconfigscheme.AddToScheme(k8sSchema)
+	err = clientgoscheme.AddToScheme(k8sSchema)
+	assert.NoError(t, err)
+
+	err = eniconfigscheme.AddToScheme(k8sSchema)
+	assert.NoError(t, err)
 
 	fakeNode := &corev1.Node{
 		ObjectMeta: v1.ObjectMeta{
 			Name: "testNode",
 		},
 	}
-	k8sClient := fake.NewFakeClientWithScheme(k8sSchema, fakeNode)
-	os.Setenv("MY_NODE_NAME", "testNode")
+	k8sClient := fake.NewClientBuilder().WithScheme(k8sSchema).WithObjects(fakeNode).Build()
+	err = os.Setenv("MY_NODE_NAME", "testNode")
+	assert.NoError(t, err)
+
 	node, err := GetNode(ctx, k8sClient)
 	assert.NoError(t, err)
 	assert.Equal(t, node.Name, "testNode")
 
-	os.Setenv("MY_NODE_NAME", "dummyNode")
+	err = os.Setenv("MY_NODE_NAME", "dummyNode")
+	assert.NoError(t, err)
+
 	_, err = GetNode(ctx, k8sClient)
 	assert.Error(t, err)
 }
