09db3603a150d0f4358baa70a05b18137bdcfedb
Jeff Nelson <jdnelson@amazon.com>
2023-09-12 11:20:40 -0500

    remove calico test suite from weekly integration tests

--- scripts/lib/integration.sh
+++ scripts/lib/integration.sh
@@ -24,39 +24,6 @@ function run_kops_conformance() {
     echo "TIMELINE: KOPS tests took $KOPS_TEST_DURATION seconds."
 
     sleep 240 #Workaround to avoid ENI leakage during cluster deletion: https://github.com/aws/amazon-vpc-cni-k8s/issues/1223
-
-}
-
-function run_calico_test() {
-  echo "Starting Helm installing Tigera operator and running Calico STAR tests"
-  pushd ./test
-  VPC_ID=$(eksctl get cluster $CLUSTER_NAME -oyaml | grep vpc | cut -d ":" -f 2 | awk '{$1=$1};1')
-
-  calico_version=$CALICO_VERSION
-  if [[ $1 == "true" ]]; then
-    # we can automatically use latest version in Calico repo, or use the known highest version (currently v3.23.0)
-    if [[ $RUN_LATEST_CALICO_VERSION == true ]]; then
-      version_tag=$(curl -i https://api.github.com/repos/projectcalico/calico/releases/latest | grep "tag_name") || true
-      if [[ -n $version_tag ]]; then
-        calico_version=$(echo $version_tag | cut -d ":" -f 2 | cut -d '"' -f 2 )
-      else
-        echo "Getting Calico latest version failed, will fall back to default/set version $calico_version instead"
-      fi
-    else echo "Using default Calico version"
-    fi
-    echo "Using Calico version $calico_version to test"
-  else
-    version=$(kubectl describe ds -n calico-system calico-node | grep "calico/node:" | cut -d ':' -f3)
-    echo "Calico has been installed in testing cluster, keep using the version $version"
-  fi
-
-  echo "Testing amd64"
-  instance_type="amd64"
-  ginkgo -v integration/calico -- --cluster-kubeconfig=$KUBECONFIG --cluster-name=$CLUSTER_NAME --aws-region=$AWS_DEFAULT_REGION --aws-vpc-id=$VPC_ID --calico-version=$calico_version --instance-type=$instance_type --install-calico=$1
-  echo "Testing arm64"
-  instance_type="arm64"
-  ginkgo -v integration/calico -- --cluster-kubeconfig=$KUBECONFIG --cluster-name=$CLUSTER_NAME --aws-region=$AWS_DEFAULT_REGION --aws-vpc-id=$VPC_ID --calico-version=$calico_version --instance-type=$instance_type --install-calico=false
-  popd
 }
 
 function build_and_push_image(){
