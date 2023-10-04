b71e525c9b1345cbab3c8fa4a5b954e01cfb973b
Jay Deokar <jsdeokar@amazon.com>
2023-10-04 15:27:06 -0700

    Update metrics helper image url based on region

--- scripts/run-cni-release-tests.sh
+++ scripts/run-cni-release-tests.sh
@@ -36,7 +36,12 @@ function run_integration_test() {
   cd $INTEGRATION_TEST_DIR/cni && CGO_ENABLED=0 ginkgo $EXTRA_GINKGO_FLAGS -v -timeout 60m --no-color --fail-on-pending -- --cluster-kubeconfig="$KUBE_CONFIG_PATH" --cluster-name="$CLUSTER_NAME" --aws-region="$REGION" --aws-vpc-id="$VPC_ID" --ng-name-label-key="$NG_LABEL_KEY" --ng-name-label-val="$NG_LABEL_VAL" --test-image-registry=$TEST_IMAGE_REGISTRY || TEST_RESULT=fail
   echo "cni test took $((SECONDS - START)) seconds."
 
-  : "${CNI_METRICS_HELPER:=602401143452.dkr.ecr.us-west-2.amazonaws.com/cni-metrics-helper:v1.13.4}"
+  if [[ ! -z $PROD_IMAGE_REGISTRY ]]; then
+    CNI_METRICS_HELPER="$PROD_IMAGE_REGISTRY/cni-metrics-helper:v1.15.0"
+  else
+    CNI_METRICS_HELPER="${CNI_METRICS_HELPER:=602401143452.dkr.ecr.us-west-2.amazonaws.com/cni-metrics-helper:v1.15.0}"
+  fi
+
   REPO_NAME=$(echo $CNI_METRICS_HELPER | cut -d ":" -f 1)
   TAG=$(echo $CNI_METRICS_HELPER | cut -d ":" -f 2)
   echo "Running cni-metrics-helper image($CNI_METRICS_HELPER) tests"
