5557443230cdb70efa96d15f5e3fde31265ca1c9
Jay Deokar <23660509+jaydeokar@users.noreply.github.com>
2023-10-11 11:15:02 -0700

    Graceful termination for service connectivity tests (#2611)

--- test/integration/cni/service_connectivity_test.go
+++ test/integration/cni/service_connectivity_test.go
@@ -35,7 +35,7 @@ const (
 )
 
 // Verifies connectivity to deployment behind different service types
-var _ = Describe("[CANARY] test service connectivity", func() {
+var _ = Describe("[CANARY] test service connectivity", FlakeAttempts(3), func() {
 	var err error
 
 	// Deployment running the http server
@@ -99,7 +99,7 @@ var _ = Describe("[CANARY] test service connectivity", func() {
 		fmt.Fprintf(GinkgoWriter, "created service\n: %+v\n", service.Status)
 
 		By("sleeping for some time to allow service to become ready")
-		time.Sleep(3 * utils.PollIntervalLong)
+		time.Sleep(utils.PollIntervalLong)
 
 		testerContainer = manifest.NewBusyBoxContainerBuilder(f.Options.TestImageRegistry).
 			Command([]string{"wget"}).
@@ -145,17 +145,26 @@ var _ = Describe("[CANARY] test service connectivity", func() {
 	})
 
 	JustAfterEach(func() {
-		err := f.K8sResourceManagers.JobManager().DeleteAndWaitTillJobIsDeleted(testerJob)
-		Expect(err).ToNot(HaveOccurred())
 
-		err = f.K8sResourceManagers.JobManager().DeleteAndWaitTillJobIsDeleted(negativeTesterJob)
-		Expect(err).ToNot(HaveOccurred())
+		if testerJob != nil {
+			err := f.K8sResourceManagers.JobManager().DeleteAndWaitTillJobIsDeleted(testerJob)
+			Expect(err).ToNot(HaveOccurred())
+		}
 
-		err = f.K8sResourceManagers.ServiceManager().DeleteAndWaitTillServiceDeleted(context.Background(), service)
-		Expect(err).ToNot(HaveOccurred())
+		if negativeTesterJob != nil {
+			err = f.K8sResourceManagers.JobManager().DeleteAndWaitTillJobIsDeleted(negativeTesterJob)
+			Expect(err).ToNot(HaveOccurred())
+		}
 
-		err = f.K8sResourceManagers.DeploymentManager().DeleteAndWaitTillDeploymentIsDeleted(deployment)
-		Expect(err).ToNot(HaveOccurred())
+		if service != nil {
+			err = f.K8sResourceManagers.ServiceManager().DeleteAndWaitTillServiceDeleted(context.Background(), service)
+			Expect(err).ToNot(HaveOccurred())
+		}
+
+		if deployment != nil {
+			err = f.K8sResourceManagers.DeploymentManager().DeleteAndWaitTillDeploymentIsDeleted(deployment)
+			Expect(err).ToNot(HaveOccurred())
+		}
 
 		// Sleep for IP cooldown period to ensure IPs are added back to datastore for future test runs
 		time.Sleep(5 * time.Second)
