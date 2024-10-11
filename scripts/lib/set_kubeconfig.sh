9b380da51f9ba6e571656be83d82d80f275b6026
Senthil Kumaran <senthilx@amazon.com>
2024-10-11 12:56:42 -0700

    Run Release Test for Updating KubeConfig. (#3069)

--- scripts/lib/set_kubeconfig.sh
+++ scripts/lib/set_kubeconfig.sh
@@ -2,6 +2,6 @@
 
 # This script sets the KUBECONFIG environment variable based on KUBE_CONFIG_PATH if not already set.
 
-if [ -n "$KUBE_CONFIG_PATH" ] && [ -z "$KUBECONFIG" ]; then
+if [ -n "${KUBE_CONFIG_PATH:-}" ] && [ -z "$KUBECONFIG" ]; then
   export KUBECONFIG=$KUBE_CONFIG_PATH
 fi
