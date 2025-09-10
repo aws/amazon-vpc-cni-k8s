963ad26bf12e16a18de34b69d7461396a066900a
Jay Deokar <jsdeokar@amazon.com>
2025-09-09 23:58:02 -0700

    Optionally setting up Route table

--- pkg/networkutils/helper.go
+++ pkg/networkutils/helper.go
@@ -2,11 +2,18 @@ package networkutils
 
 import "net"
 
-func CalculateRouteTableId(deviceNumber int, networkCardIndex int, maxENIsPerNetworkCard int) int {
+const BaseNumber = 10000
+
+// func CalculateOldRouteTableId(deviceNumber int, networkCardIndex int, maxENIsPerNetworkCard int) int {
+// 	return deviceNumber + 1 + (networkCardIndex * maxENIsPerNetworkCard)
+// }
+
+func CalculateRouteTableId(deviceNumber int, networkCardIndex int) int {
 	if networkCardIndex == 0 {
 		return deviceNumber + 1
 	} else {
-		return deviceNumber + 1 + (networkCardIndex * maxENIsPerNetworkCard)
+		// https://github.com/amazonlinux/amazon-ec2-net-utils/blob/main/lib/lib.sh#L285
+		return BaseNumber + deviceNumber + (100 * networkCardIndex)
 	}
 }
 
